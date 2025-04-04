d19e86a6518e76177318771d96235577bc24fb16
Hudsondaniel <hudsondaniel321@gmail.com>
2025-04-04 12:44:17 +0530

    Feat: Completed!

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const port = 3030;
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 app.use("/", indexRouter);
 app.listen(port, () => { 
     console.log(`✅ Server running on http://localhost:${port}`);
